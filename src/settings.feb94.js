acfdc36eb0e889915b39bc636d702cf363be3c41
Huuthaia2 <huuthaia2@gmail.com>
2023-07-28 08:10:56 +0700

    z

---
+++ src/settings.feb94.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/res/scenes/LoadingScene.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"247c5",scriptsBundle:"a8cd9",main:"4effa"}};
